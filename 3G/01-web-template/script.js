1f73ae0d586aea44c227827c2276f8e0ef58b5c7
Th1nk <Tobiasransborg@hotmail.com>
2024-08-23 12:51:33 +0200

    min-favoritbog
    Web app øvelse færdig done finally

--- 3G/01-web-template/script.js
+++ 3G/01-web-template/script.js
@@ -3,9 +3,14 @@ let pages
 function setup(){
     console.log('P5.js er loadedd')
     pages = selectAll('.page')
+    select('#item1').mouseClicked( () => shiftPage('1') )
+    select('#item2').mouseClicked( () => shiftPage('2') )
+    select('#item3').mouseClicked( () => shiftPage('3') )
+    select('#item4').mouseClicked( () => shiftPage('4') )
 }
 
 function shiftPage(num){
+    /*
     if(num == 'ArrowRight'){
         num = currentPage + 1
     }
@@ -15,11 +20,13 @@ function shiftPage(num){
     if(isNaN(num) || num <= 0 || num > pages.length){
         return
     }
+    */
     select("#page"+currentPage).removeClass('visible')
     currentPage = num
     select("#page"+currentPage).addClass('visible')
 }
-
+/*
 function keyPressed(){
     shiftPage(key)
 }
+*/
