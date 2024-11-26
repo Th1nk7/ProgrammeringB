b25c30a02fbae3911afe2de4153c95e4ca3692a1
Th1nk <Tobiasransborg@hotmail.com>
2024-11-26 21:57:51 +0100

    clocks and shit

--- 3G/12_OOP_Clock_klassen/Clock.js
+++ 3G/12_OOP_Clock_klassen/Clock.js
@@ -4,58 +4,74 @@ console.log('Clock is here')
 //Kaldes det POLYMORFI
 class Clock {
     //constructoren er klassens "setup" funktion, som kaldes når nye objekter fra klassen oprettes 
-    constructor(div, style){
-        this.div = div
-        this.style = style
-        //div's for hours, minutes, seconds
+    constructor(type, bgColor, txtColor, alarmTime){
+        this.alarmSound = alarmSound
+        this.alarmTime = alarmTime
+        this.sStart = second()
+        this.type = type
+        this.div = createDiv()
+        this.div.parent(select( this.type == 'clock' ? '#clocksDiv' : '#alarmsDiv'))
+        this.bgColor = bgColor
+        this.txtColor = txtColor
         this.hDiv = createDiv()
         this.mDiv = createDiv()
         this.sDiv = createDiv()
+        this.btnDiv = createDiv()
         this.div.child(this.hDiv)
         this.div.child(this.mDiv)
         this.div.child(this.sDiv)
-        //interval til at sætte tiden ind 
+        this.div.child(this.btnDiv)
         this.interval
-        //styles 
+        this.btn = createButton("Slet")
+        this.btn.parent(this.btnDiv)
+        this.btn.addClass('rmBtn')
+        this.btnDiv.addClass('rmBtnDiv')
+        this.btn.mousePressed(() => {
+            this.stop()
+            this.div.remove()
+            clocks.pop(clocks[clocks.findIndex((ele) => ele == null)])
+            alarms.pop(clocks[clocks.findIndex((ele) => ele == null)])
+        })
         this.div.style(
             `
             width:24rem;
             height:7.5rem;
-            border: 4px dotted gray;
+            border: 4px dotted color-mix(in srgb, gray 60%, ${this.txtColor} 40%);
             display:grid;
             grid-template-columns: 1fr 1fr 1fr;
             padding: 1rem;
             border-radius: 2rem;
             place-items:center;
             font-size:2.5rem;
+            background:${this.bgColor};
+            color:${this.txtColor}
             `
         )
-
-        //reager på argumentet style fra constructoren
-         switch(style){
-            case 'pink': 
-                this.div.style('background', 'hotpink')
-                break
-            case 'black':
-                this.div.style('background', 'black')
-                this.div.style('color', 'white')
-                break
-            default:
-                this.div.style('background', 'rgba(0,0,0,0)')
-         }
 
         
     }
     start(){
-        this.interval = setInterval( ()=>{
-            //den her komapkte linje kode, betyder at vi SPØRGER om hour() funktionen returnerer 
-            //et tal UNDER ti - hvis ja, sætter vi et nul foran 
-            this.hDiv.html( hour() < 10 ? '0' + hour() : hour() )
-            this.mDiv.html( minute() < 10 ? '0' + minute() : minute() )
-            this.sDiv.html( second() < 10 ? '0' + second() : second() )
-        }, 1000)
+        if (this.type == 'clock') {
+            this.interval = setInterval( ()=>{
+                this.hDiv.html( hour() < 10 ? '0' + hour() : hour() )
+                this.mDiv.html( minute() < 10 ? '0' + minute() : minute() )
+                this.sDiv.html( second() < 10 ? '0' + second() : second() )
+            }, 1000)
+        }
+        else {
+            this.interval = setInterval( () => {
+                this.hDiv.html( this.alarmTime[0] + this.alarmTime[1])
+                this.mDiv.html( this.alarmTime[3] + this.alarmTime[4])
+                this.sDiv.html( this.alarmTime[6] + this.alarmTime[7])
+                if(this.alarmTime[0] + this.alarmTime[1] == ( hour() < 10 ? '0' + hour() : hour() ) && this.alarmTime[3] + this.alarmTime[4] == ( minute() < 10 ? '0' + minute() : minute() ) && this.alarmTime[6] + this.alarmTime[7] == ( second() < 10 ? '0' + second() : second() )) {
+                    this.stop()
+                    this.alarmSound.play()
+                }
+            }, 500)
+        }
     }
     stop(){
         clearInterval(this.interval)
+        if(this.type == "alarm") {this.alarmSound.stop()}
     }
 }
--- 3G/12_OOP_Clock_klassen/index.js
+++ 3G/12_OOP_Clock_klassen/index.js
@@ -1,17 +1,51 @@
 let clocks = []
+let alarms = []
 let maxClocks = 9
+let maxAlarms = 9
+let alarmSound
+
+function preload(){
+    alarmSound = loadSound('https://cdn.pixabay.com/audio/2024/11/20/audio_659d575689.mp3')
+}
 
 function setup(){
+    timePicker = createInput('00:00:00', 'time')
+    timePicker.id('timePicker')
+    timePicker.style("font-size: 20px")
+    timePicker.center()
+    timePicker.position(windowWidth/100*73.75,windowHeight/5)
+    timePicker.attribute('step', 2)
+
+    bgColor = createColorPicker('deeppink')
+    bgColor.center()
+    bgColor.position(windowWidth/100*19.5,windowHeight/7)
+    
+    txtColor = createColorPicker('black')
+    txtColor.center()
+    txtColor.position(windowWidth/100*24.1,windowHeight/7)
+    
+    typeSelect = createSelect()
+    typeSelect.center()
+    typeSelect.id('typeSelect')
+    typeSelect.position(windowWidth/100*74.75,windowHeight/7)
+    typeSelect.option('Ur')
+    typeSelect.option('Alarm')
+    typeSelect.selected('Ur')
+    typeSelect.changed(() => timePicker.toggleClass('show'))
+    
     newBtn = createButton("Kilk mig!")
     newBtn.center()
+    newBtn.id('newBtn')
     newBtn.position(this.position,windowHeight/4)
     newBtn.style("background-color:limegreen;color:black;border-color:black;")
     newBtn.mousePressed(() => {
-        if(clocks.length < max(maxClocks)){
-            div = createDiv()
-            div.parent(select('#clocksDiv'))
-            append(clocks, new Clock(div))
+        if(typeSelect.selected() == "Ur" && clocks.length < max(maxClocks)) {
+            append(clocks, new Clock('clock', bgColor.value(), txtColor.value()))
             clocks[clocks.length-1].start()
+        }
+        else if(typeSelect.selected() == "Alarm" && alarms.length < max(maxAlarms)) {
+            append(alarms, new Clock('alarm', bgColor.value(), txtColor.value(), timePicker.value()))
+            alarms[alarms.length-1].start()
         }
     })
 }
